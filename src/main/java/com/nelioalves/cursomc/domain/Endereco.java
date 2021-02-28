5035c2a088b33f9551ce84673bfece92856b5bee
Alex Cesar Nascimento <66449173+AlexCesarNascimento@users.noreply.github.com>
2021-02-27 23:55:18 -0300

    Cliente, TipoCliente, Telefones e Endereços

--- src/main/java/com/nelioalves/cursomc/domain/Endereco.java
+++ src/main/java/com/nelioalves/cursomc/domain/Endereco.java
@@ -1,32 +1,48 @@
 package com.nelioalves.cursomc.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Endereco {
-	
+public class Endereco implements Serializable{
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String logradouro;
-	private Integer numero;
+	private String numero;
 	private String complemento;
 	private String bairro;
 	private String cep;
 	
+	@ManyToOne
+	@JoinColumn(name="cliente_id")
+	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(name="cidade_id")
+	private Cidade cidade;
+	
+	
 	public Endereco() {
 	}
 
-	public Endereco(Integer id, String logradouro, Integer numero, String complemento, String bairro, String cep) {
+	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep, Cliente cliente, Cidade cidade) {
 		this.id = id;
 		this.logradouro = logradouro;
 		this.numero = numero;
 		this.complemento = complemento;
 		this.bairro = bairro;
 		this.cep = cep;
+		this.setCliente(cliente); 
+		this.setCidade(cidade);
 	}
 
 	public Integer getId() {
@@ -45,11 +61,11 @@ public class Endereco {
 		this.logradouro = logradouro;
 	}
 
-	public Integer getNumero() {
+	public String getNumero() {
 		return numero;
 	}
 
-	public void setNumero(Integer numero) {
+	public void setNumero(String numero) {
 		this.numero = numero;
 	}
 
@@ -76,6 +92,22 @@ public class Endereco {
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
 
 	@Override
 	public int hashCode() {
@@ -101,7 +133,4 @@ public class Endereco {
 			return false;
 		return true;
 	}
-	
-	
-
 }
